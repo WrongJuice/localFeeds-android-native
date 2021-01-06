e790e6549edf92a32d436c47786f6a339afa4a93
Lucien Barçon <lucien@teamsmart.io>
2021-01-06 14:00:00 +0100

    Affichage vue liste producteur

--- app/src/main/java/com/example/localfeeds/activities/ListeProducteursActivity.java
+++ app/src/main/java/com/example/localfeeds/activities/ListeProducteursActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.localfeeds.R;
+
 public class ListeProducteursActivity extends AppCompatActivity {
 
     @Override
