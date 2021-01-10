702349bc90cc1e5f12739b8a37ecfb2fd41c8a54
Lucien Barçon <lucien@teamsmart.io>
2021-01-10 17:18:44 +0100

    Ajout d'id plus ressources dans string xml

--- app/src/main/java/com/example/localfeeds/activities/InformationProducteur.java
+++ app/src/main/java/com/example/localfeeds/activities/InformationProducteur.java
@@ -9,6 +9,7 @@ import android.transition.TransitionManager;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.localfeeds.R;
 
@@ -22,9 +23,15 @@ public class InformationProducteur extends AppCompatActivity {
         Bundle parametres = this.getIntent().getExtras();
         String parametreId_producteur = (String) parametres.get("id_producteur");
 
+        // RECUPERER LE PRODUCTEUR
+
         ImageButton arrow = findViewById(R.id.arrow_button);
         LinearLayout hiddenView = findViewById(R.id.hidden_view);
         CardView cardView = findViewById(R.id.base_cardview);
+
+        TextView champAdresse = findViewById(R.id.vue_information_producteur_champ_adresse);
+        TextView champHoraires = findViewById(R.id.vue_information_producteur_champ_horaires);
+        TextView champDescription = findViewById(R.id.vue_information_producteur_champ_description);
 
         arrow.setOnClickListener(new View.OnClickListener() {
             @Override
