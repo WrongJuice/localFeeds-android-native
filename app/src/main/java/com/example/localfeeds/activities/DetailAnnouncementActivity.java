3c736c12547643580b178c27d00da896b3954b77
Alfred GAILLARD <alfred.gaillard.pro@gmail.com>
2021-01-13 14:45:18 +0100

    Linking the model

--- app/src/main/java/com/example/localfeeds/activities/DetailAnnouncementActivity.java
+++ app/src/main/java/com/example/localfeeds/activities/DetailAnnouncementActivity.java
@@ -3,14 +3,22 @@ package com.example.localfeeds.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.localfeeds.R;
 
 public class DetailAnnouncementActivity extends AppCompatActivity {
 
+    TextView titleTextView;
+    TextView descriptionTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_announcement);
+        titleTextView = findViewById(R.id.title);
+        descriptionTextView = findViewById(R.id.description);
+        titleTextView.setText(getIntent().getStringExtra("announcement_title"));
+        descriptionTextView.setText(getIntent().getStringExtra("announcement_desc"));
     }
 }
